1b0e3e6486f60004daddc1073823661dd82f88c0
Frances Koshoedo <bkoshoedo@yahoo.com>
2024-01-15 13:52:41 -0700

    1st Commit

--- src/test/java/StepDefs/TestRunners.java
+++ src/test/java/StepDefs/TestRunners.java
@@ -1,2 +1,17 @@
-package StepDefs;public class TestRunners {
+package StepDefs;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/main/resources/feature_files",
+        plugin = { "pretty", "html:target/cucumber.html",
+                "json:target/cucumber/cucumber-report.json",
+                "junit:target/cucumber.xml"},
+        tags = "@UKBA")
+
+public class TestRunners {
+
+
 }
